54b884754a2e0e7d223641911bdb4e63a5e98e7c
Grzegorz Dryja <grzegorz.dryja@outlook.com>
2022-06-03 22:17:48 +0200

    Input Component
    First Angular Material component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
