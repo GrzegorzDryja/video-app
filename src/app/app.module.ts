0437bdcfe79dd3dbf18a969def4168f3d7e76d64
Grzegorz Dryja <grzegorz.dryja@outlook.com>
2022-06-05 00:03:23 +0200

    Getting inut
    Handling submit button with Angular Forms, some responsive clean ups.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
@@ -16,7 +16,8 @@ import { InputComponent } from './input/input.component';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
@@ -12,4 +13,7 @@ export class InputComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onAddVideo(form: NgForm){
+    console.log(form.value.video);
+  }
 }
