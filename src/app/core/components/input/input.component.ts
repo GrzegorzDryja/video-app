f837d2cf00776490f317dbaeae8ed86ef2cb53ef
GrzegorzDryja <grzegorz.dryja@outlook.com>
2022-09-09 10:16:24 +0200

    Input component improvements

--- src/app/core/components/input/input.component.ts
+++ src/app/core/components/input/input.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 import { UserInputService } from '@services/user-input.service';
 import { YoutubeService } from '@services/youtube.service';
 import { VimeoService } from '@services/vimeo.service'
+import { Platform } from '@shared/platform.model';
 
 @Component({
   selector: 'app-input',
@@ -18,22 +20,22 @@ export class InputComponent {
     private vimeo: VimeoService
   ){}
 
-  onAddVideo(form: NgForm): void{
+  public onAddVideo(form: NgForm): Subscription | null {
     if (this.userInput.validatePath(form.form.value.video)){
-      let dataToFetch = {
+      const dataToFetch = {
         platform: this.userInput.extractPlatform(form.form.value.video),
         id: this.userInput.extractId(form.form.value.video)
       }
-    if (dataToFetch.platform == "youtube"){
-        this.youtube.fetchVideo(`${dataToFetch.id}`)
-      }
-    if (dataToFetch.platform == "vimeo"){
-        this.vimeo.fetchVideo(`${dataToFetch.id}`)
-      }
-    } else {
 
-      console.log("Nie obsługuję tego linku lub link zawiera błędy!")
+      if (dataToFetch.platform === Platform.youtube){
+        return this.youtube.fetchVideo(`${dataToFetch.id}`)
+      }
 
+      if (dataToFetch.platform === Platform.vimeo){
+        return this.vimeo.fetchVideo(`${dataToFetch.id}`)
+      }
     }
+
+    return null;
   }
 }
