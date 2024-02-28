320cb38ce3533ed60cca87068f1717aaf562be7c
Grzegorz Dryja <grzegorz.dryja@outlook.com>
2024-02-28 14:14:51 +0100

    fix: folder structure refactor

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,11 +5,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from '@app/shared/shared.module';
 import { InputComponent } from './components/input/input.component';
-import { MenuComponent } from './components/video-list/list/menu/menu.component';
-import { VideoListComponent } from './components/video-list/list/video-list.component';
+import { MenuComponent } from './components/video-list/menu/menu.component';
+import { VideoListComponent } from './components/video-list/video-list.component';
 import { ItemComponent } from './components/video-list/item/item.component';
-import { FavoritePipe } from './components/video-list/list/pipe/favorite.pipe';
-import { SortPipe } from './components/video-list/list/pipe/sort.pipe';
+import { FavoritePipe } from './components/video-list/pipe/favorite.pipe';
+import { SortPipe } from './components/video-list/pipe/sort.pipe';
 
 const CORE_COMPONENTS = [
   InputComponent,
@@ -18,10 +18,9 @@ const CORE_COMPONENTS = [
   ItemComponent,
 ];
 
-const CORE_PIPES = [
-  FavoritePipe,
-  SortPipe
-]
+const EXPORT_COMPONENTS = [InputComponent, VideoListComponent];
+
+const CORE_PIPES = [FavoritePipe, SortPipe];
 
 @NgModule({
   declarations: [...CORE_COMPONENTS, ...CORE_PIPES],
@@ -30,8 +29,8 @@ const CORE_PIPES = [
     BrowserAnimationsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    SharedModule
+    SharedModule,
   ],
-  exports: [InputComponent, VideoListComponent]
+  exports: [...EXPORT_COMPONENTS],
 })
 export class CoreModule {}
